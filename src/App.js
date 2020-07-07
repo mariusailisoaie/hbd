429a26eb26458ca9275b9022ef9703d383ec062d
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2020-07-07 19:12:27 +0200

    Update Fireworks import path. Import Confetti component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import FireworksComponent from './FireworksComponent';
+import Fireworks from './components/Fireworks';
+import Confetti from './components/Confetti';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Happy Birthday Dani!</h1>
-        <FireworksComponent />
+        <Fireworks />
+        <Confetti />
       </header>
     </div>
   );
